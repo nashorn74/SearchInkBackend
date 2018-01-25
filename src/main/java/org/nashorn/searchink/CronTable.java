6a281352d723e20800ddafb3c17aa15682588b3e
Jeong kum ho <nashorn74@gmail.com>
2018-01-25 23:45:11 +0900

    add auto perform function

--- src/main/java/org/nashorn/searchink/CronTable.java
+++ src/main/java/org/nashorn/searchink/CronTable.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.time.DateUtils;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -36,11 +37,38 @@ public class CronTable {
 		        taskRepository.save(task);
 			}
 			
-			List<Task> tasks = taskRepository.findAllExceptPostpone();
-			webSocket.convertAndSend("/topic/greetings", 
-					tasks.stream().map(task -> modelMapper.map(task, TaskDto.Task.class))
-			          .collect(Collectors.toList()));
+			sendTaskList();
 		}
+		
+		List<Task> dueTasks = taskRepository.findFirstForDue();
+		if (dueTasks.size() == 0) {
+			List<Task> performTasks = taskRepository.findFirstForPerform();
+			if (performTasks.size() > 0) {
+				Task task = performTasks.get(0);
+				Date dueDate = DateUtils.addMinutes(new Date(), 2);
+				task.setUpdatedat(new Date());
+				task.setDuedate(dueDate);
+				task.setResolvedat(null);
+				task.setStatus(8888);
+				taskRepository.save(task);
+				System.out.println("new perform task:"+task.getId());
+				
+				sendTaskList();
+			}
+		} else {
+			List<Task> overDueTasks = taskRepository.findFirstOverDue(new Date());
+			if (overDueTasks.size() > 0) {
+				Task task = overDueTasks.get(0);
+				task.setUpdatedat(new Date());
+				task.setResolvedat(new Date());
+				task.setStatus(9999);
+				taskRepository.save(task);
+				System.out.println("complete task:"+task.getId());
+				
+				sendTaskList();
+			}
+		}
+		
 	}
 	
 	@Scheduled(initialDelay = 60000, fixedDelay = 60000) 
@@ -49,6 +77,10 @@ public class CronTable {
 		taskRepository.save(new Task(new Date(), null, null, null, 
         		"auto-task", "auto-task description", 1, 1));
 		
+		sendTaskList();
+	}
+	
+	public void sendTaskList() {
 		List<Task> tasks = taskRepository.findAllExceptPostpone();
 		webSocket.convertAndSend("/topic/greetings", 
 				tasks.stream().map(task -> modelMapper.map(task, TaskDto.Task.class))
--- src/main/java/org/nashorn/searchink/TaskRepository.java
+++ src/main/java/org/nashorn/searchink/TaskRepository.java
@@ -3,17 +3,29 @@ package org.nashorn.searchink;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.QueryHint;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 	public Task findOneById(String id);
 	
-	@Query("select a from Task a where a.duedate = NULL and a.resolvedat = NULL")
+	@Query("select a from Task a where a.resolvedat = NULL")
 	public List<Task> findAllExceptPostpone();
 	
 	@Query("select a from Task a where a.duedate = NULL and a.resolvedat < ?1")
 	public List<Task> findAllOverPostpone(Date now);
+	
+	@Query("select a from Task a where a.duedate <> NULL and a.resolvedat = NULL")
+	public List<Task> findFirstForDue();
+	
+	@Query("select a from Task a where a.duedate < ?1 and a.resolvedat = NULL")
+	public List<Task> findFirstOverDue(Date now);
+	
+	@Query("select a from Task a where a.duedate = NULL and a.resolvedat = NULL ORDER BY a.createdat ASC")
+	public List<Task> findFirstForPerform();
 }
